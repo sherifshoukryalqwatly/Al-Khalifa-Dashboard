62eb1f274f7394a55e0b228f84127bd6554c0a7d
sherifshoukryalqwatly <shoukrysherif5@gmail.com>
2025-12-13 13:44:37 +0200

    feat(Auth): add login , forget-password and reset-password  Page layout

--- src/app/features/auth/pages/reset-password/reset-password.ts
+++ src/app/features/auth/pages/reset-password/reset-password.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-reset-password',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './reset-password.html',
   styleUrl: './reset-password.css',
 })
