432ed342213be48bcd411096ae65039e5da82eab
sherifshoukryalqwatly <shoukrysherif5@gmail.com>
2025-12-13 13:08:24 +0200

    feat(Settings): add Settings Page layout

--- src/app/features/settings/pages/general/general.ts
+++ src/app/features/settings/pages/general/general.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-general',
-  imports: [],
+  imports: [ RouterModule],
   templateUrl: './general.html',
   styleUrl: './general.css',
 })
 export class General {
 
+  constructor(private router: Router) {}
+
+  testNav() {
+    this.router.navigate(['/settings/store']);
+  }
 }
