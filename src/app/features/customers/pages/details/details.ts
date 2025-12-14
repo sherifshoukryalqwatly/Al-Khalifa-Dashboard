263fd4d5d3a17f1577a0b77ee2f5142c86033718
sherifshoukryalqwatly <shoukrysherif5@gmail.com>
2025-12-14 10:02:29 +0200

    feat(Users): adding user Details Page layout

--- src/app/features/customers/pages/details/details.ts
+++ src/app/features/customers/pages/details/details.ts
@@ -1,11 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-details',
-  imports: [],
+  imports: [CommonModule,RouterModule],
   templateUrl: './details.html',
   styleUrl: './details.css',
 })
 export class CustomerDetails {
-
+  user = {
+    name: 'Ahmed Ali',
+    email: 'ahmed.ali@example.com',
+    role: 'Admin',
+    phone: '+20 100 123 4567',
+    active: true,
+    createdAt: '2024-06-15',
+    lastLogin: '2025-12-12',
+    avatar: 'https://i.pravatar.cc/150?img=12'
+  };
 }
