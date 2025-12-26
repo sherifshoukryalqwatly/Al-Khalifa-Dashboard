5b32245ddfa2d653ded22a1aed65dd701d1049c3
sherifshoukryalqwatly <shoukrysherif5@gmail.com>
2025-12-27 02:54:56 +0300

    feat(footer): adding footer layout

--- src/app/layouts/main-layout/main-layout.ts
+++ src/app/layouts/main-layout/main-layout.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component,Inject,OnInit, PLATFORM_ID  } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from '../../shared/components/navbar/navbar';
 import { Sidebar } from '../../shared/components/sidebar/sidebar';
 import { Footer } from "../../shared/components/footer/footer";
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-main-layout',
@@ -10,6 +11,20 @@ import { Footer } from "../../shared/components/footer/footer";
   templateUrl: './main-layout.html',
   styleUrl: './main-layout.css',
 })
-export class MainLayout {
+export class MainLayout implements OnInit {
+  isSidebarCollapsed = false;
 
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+
+  ngOnInit(): void {
+  if (isPlatformBrowser(this.platformId)) {
+    if (window.innerWidth < 768) {
+      this.isSidebarCollapsed = true;
+    }
+  }
+}
+
+  onSidebarToggle(collapsed: boolean) {
+    this.isSidebarCollapsed = collapsed;
+  }
 }
--- src/app/shared/components/sidebar/sidebar.ts
+++ src/app/shared/components/sidebar/sidebar.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { AnalyticsRoutingModule } from "../../../features/analytics/analytics-routing-module";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [AnalyticsRoutingModule],
+  imports: [AnalyticsRoutingModule,CommonModule],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.css',
 })
 export class Sidebar {
+  isCollapsed = false;
 
+  @Output() collapsedChange = new EventEmitter<boolean>();
+
+  toggleSidebar() {
+    this.isCollapsed = !this.isCollapsed;
+    this.collapsedChange.emit(this.isCollapsed);
+  }
+
+  menu = [
+    { label: 'Dashboard', link: 'dashboard', icon: 'bi-speedometer2' },
+    { label: 'Analytics', link: '/analytics', icon: 'bi-bar-chart' },
+    { label: 'Products', link: '/products', icon: 'bi-box' },
+    { label: 'Users', link: '/customers', icon: 'bi-people' },
+    { label: 'Categories', link: '/categories', icon: 'bi-grid' },
+    { label: 'Orders', link: '/orders', icon: 'bi-cart' },
+    { label: 'Coupons', link: '/coupons', icon: 'bi-tag' },
+    { label: 'Inventory', link: '/inventory', icon: 'bi-box-seam' },
+    { label: 'Payments', link: '/payments', icon: 'bi-credit-card' },
+    { label: 'Reviews', link: '/reviews', icon: 'bi-star' },
+    { label: 'Shipping', link: '/shipping', icon: 'bi-truck' },
+    { label: 'Settings', link: '/settings', icon: 'bi-gear' },
+  ];
 }
